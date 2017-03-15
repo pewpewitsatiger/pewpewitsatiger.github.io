c3cf0e2743199f47eebbd882fd31e1efe7cd91eb
pewpewitsatiger <J_montalvo@icloud.com>
2017-03-14 23:39:20 -0400

    Add files via upload

---
+++ puppies/scripts/main.js
@@ -0,0 +1,26 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === '/home/jaydon/Pictures/rottweiler-puppy2.jpg') {
+      myImage.setAttribute ('src','/home/jaydon/Pictures/rottweiler-puppy2.jpg');
+    } else {
+      myImage.setAttribute ('src','/home/jaydon/Pictures/rottweiler-puppy2.jpg');
+    }
+}
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Puppies are cool, ' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Puppies are cool, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
